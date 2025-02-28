601649c5c0173046901f99cea3e2432257bab05c
Armaan singh <armaansinghlambhha@gmail.com>
2025-02-28 20:48:25 +0530

    next auth url update

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
           </div>
 
           <div className="right flex flex-col w-full gap-4 justify-center items-center bg-green-100">
-            <h1 className="text-5xl flex flex-col items-center"><span>Find your next <b>BIG</b> thing to </span><span className="font-bold text-blue-500">contribute</span></h1>
+            <h1 className="text-5xl flex flex-col items-center"><span>Find your next <b>BIG</b> thing to </span><span className="font-bold text-blue-500">contribute!</span></h1>
             <p className="text-2xl">Find projects with<b> now</b></p>
             <button className="bg-green-500 hover:bg-green-600 transition text-white p-3 rounded " onClick={() => router.push("/dashboard")}>Search project</button>
           </div>
